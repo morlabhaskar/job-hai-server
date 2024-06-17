0d69bf2c681dd5ce19bc5fcbed36c14ff42fd25a
morlabhaskar <morlabhaskar306@gmail.com>
2024-06-17 19:38:55 +0530

    url updated

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://job-hai-server.vercel.app"],
+        origin: ["https://job-hai-server.vercel.app"],
         methods: ["GET,POST,DELETE,PUT,PATCH"],
         credentials: true,
     })
